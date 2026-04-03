7c078f0d5dfe979c854d14c2bec6572c8838d534
paszed <edvard.pasz@icloud.com>
2026-04-03 17:52:48 +0200

    init: basic CLI that reads JSON from stdin

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+async function readStdin() {
+  return new Promise((resolve) => {
+    let data = "";
+
+    process.stdin.on("data", chunk => data += chunk);
+    process.stdin.on("end", () => {
+      try {
+        resolve(JSON.parse(data));
+      } catch {
+        console.error("Invalid JSON input");
+        process.exit(1);
+      }
+    });
+  });
+}
+
+const input = await readStdin();
+console.log(JSON.stringify(input, null, 2));
